1f142f10662e6300576c502882c3d2af15f3819a
IAF <akritimsingh@gmail.com>
2024-09-02 01:12:58 +0530

    initial push

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+    <div className='header'>Header</div>
+    <table style={{width:'100%',height:'628.5px',borderSpacing:'0px'}}>
+      <thead>
+        <th className='Nav'>Navigation</th>
+        <th className='Main'>Main</th>
+        <th className='Sidebar'>Sidebar</th>
+      </thead>
+    </table>
+    <div className='footer'>Footer</div>
+    </div>
+  );
+}
+
+export default App;
